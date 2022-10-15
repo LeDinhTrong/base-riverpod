bde2edb8d3f2d034e03a8c9618d8d62ca5267f09
Le Dinh Trong <trong.le@pamobile.co>
2022-10-15 23:15:26 +0700

    second commit

--- lib/page.dart
+++ lib/page.dart
@@ -1,8 +1,9 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'data/model/item_model.dart';
 import 'provider/provider.dart';
-// import 'provider/provider.dart';
 
 List<String> sliderList = [
   'https://mcpeaddons.com/wp-content/uploads/2022/08/dream-smp-skin-pack-4.jpg',
@@ -22,122 +23,112 @@ class HomePage extends HookConsumerWidget {
 
     return Scaffold(
       backgroundColor: theme.backgroundColor,
-      // body: ItemState.,
-      // Column(
-      //   children: [
-      //     //categories
-      //     Expanded(
-      //       child: ListView.builder(
-      //           itemCount: categories.length,
-      //           shrinkWrap: true,
-      //           itemBuilder: (BuildContext context, int index) {
-      //             return itemCategory(
-      //                 context, categories[index].categoryName!, itemsMap[categories[index].categoryId], ref);
-      //           }),
-      //     ),
-      //   ],
-      // ),
+      body: itemState.maybeWhen(
+        orElse: () {},
+        loaded: (itemModel) {
+          return itemCategory(context, itemModel, ref);
+        },
+      ),
     );
   }
 
-  itemCategory(BuildContext context, String categoryTitle, List<ItemModel>? items, WidgetRef ref) {
+  itemCategory(BuildContext context, List<ItemModel>? items, WidgetRef ref) {
     final heightScreen = MediaQuery.of(context).size.height;
     final widthScreen = MediaQuery.of(context).size.width;
-    return SizedBox(
-      height: heightScreen * .22,
-      child: ListView.builder(
-          itemCount: items!.length,
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (BuildContext context, int index) {
-            return SizedBox(
-              width: widthScreen * .75,
-              child: InkWell(
-                onTap: () {},
-                child: Card(
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(7)),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        flex: 4,
-                        child: Image.network(
-                          items[index].thumbUrl!,
-                          loadingBuilder: (context, url, _) =>
-                              Image.asset('assets/placeholder_error.png', fit: BoxFit.fill),
-                          errorBuilder: (context, url, error) =>
-                              Image.asset('assets/placeholder_error.png', fit: BoxFit.fill),
-                          fit: BoxFit.cover,
-                          height: double.infinity,
-                          width: double.infinity,
-                        ),
+    return ListView.builder(
+        physics: NeverScrollableScrollPhysics(),
+        itemCount: items!.length,
+        itemBuilder: (BuildContext context, int index) {
+          log('${items[index].thumbUrl}');
+          return SizedBox(
+            height: 120,
+            width: double.infinity,
+            child: InkWell(
+              onTap: () {},
+              child: Card(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(7)),
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 4,
+                      child: Image.network(
+                        items[index].thumbUrl!,
+                        loadingBuilder: (context, url, _) =>
+                            Image.asset('assets/placeholder_error.png', fit: BoxFit.fill),
+                        errorBuilder: (context, url, error) =>
+                            Image.asset('assets/placeholder_error.png', fit: BoxFit.fill),
+                        fit: BoxFit.cover,
+                        height: double.infinity,
+                        width: double.infinity,
                       ),
-                      Expanded(
-                        flex: 6,
-                        child: Padding(
-                          padding: const EdgeInsets.all(6.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              //item name & author name
-                              RichText(
-                                text: TextSpan(
-                                  children: [
-                                    textData(
-                                      items[index].itemName!,
-                                      "name",
-                                      colorName: Colors.blueGrey,
-                                    ),
-                                    const TextSpan(
-                                      text: ' by ',
-                                      style: TextStyle(
-                                        color: Colors.grey,
-                                      ),
-                                    ),
-                                    textData(
-                                      items[index].authorName!,
-                                      "name",
-                                      colorName: Colors.blueGrey,
-                                    ),
-                                  ],
-                                ),
-                                maxLines: 4,
-                                overflow: TextOverflow.ellipsis,
-                                textAlign: TextAlign.left,
-                                softWrap: true,
-                              ),
-
-                              //short description
-                              Container(
-                                alignment: Alignment.topLeft,
-                                padding: const EdgeInsets.only(top: 2),
-                                child: RichText(
-                                  text: textData(items[index].shortDescription!, "desc"),
-                                  maxLines: 3,
-                                  overflow: TextOverflow.ellipsis,
-                                  textAlign: TextAlign.left,
-                                ),
-                              ),
-
-                              const Spacer(),
-
-                              //
-                              Row(
-                                crossAxisAlignment: CrossAxisAlignment.end,
+                    ),
+                    Expanded(
+                      flex: 6,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            //item name & author name
+                            RichText(
+                              text: TextSpan(
                                 children: [
-                                  const Icon(Icons.downloading_outlined),
-                                  Text(items[index].downloadCount.toString()),
+                                  textData(
+                                    items[index].itemName!,
+                                    "name",
+                                    colorName: Colors.blueGrey,
+                                  ),
+                                  const TextSpan(
+                                    text: ' by ',
+                                    style: TextStyle(
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                  textData(
+                                    items[index].authorName!,
+                                    "name",
+                                    colorName: Colors.blueGrey,
+                                  ),
                                 ],
                               ),
-                            ],
-                          ),
+                              maxLines: 4,
+                              overflow: TextOverflow.ellipsis,
+                              textAlign: TextAlign.left,
+                              softWrap: true,
+                            ),
+
+                            //short description
+                            Container(
+                              alignment: Alignment.topLeft,
+                              padding: const EdgeInsets.only(top: 2),
+                              child: RichText(
+                                text: textData(items[index].shortDescription!, "desc"),
+                                maxLines: 3,
+                                overflow: TextOverflow.ellipsis,
+                                textAlign: TextAlign.left,
+                              ),
+                            ),
+
+                            const Spacer(),
+
+                            //
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                const Icon(Icons.downloading_outlined),
+                                Text(items[index].downloadCount.toString()),
+                              ],
+                            ),
+                          ],
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
-            );
-          }),
-    );
+            ),
+          );
+        });
   }
 
   TextSpan textData(String data, String type, {Color? colorName}) {
